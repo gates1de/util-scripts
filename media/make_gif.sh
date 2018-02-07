26a08f2ec0c09ff96b1350a2d18c3fa4684f8772
gates1de <g031i043@gmail.com>
2018-02-07 22:45:19 +0900

    Modify make_gif.sh

--- media/make_gif.sh
+++ media/make_gif.sh
@@ -6,4 +6,17 @@ WORK_DIR=$(pwd)
 
 FILE_PATH=$WORK_DIR/$FILE_NAME
 
-ffmpeg -i $FILE_PATH.mov -vf fps=20,scale=iw/2:ih/2,palettegen=stats_mode=diff -y $FILE_PATH.png && ffmpeg -i $FILE_PATH.mov -i $FILE_PATH.png -lavfi fps=20,scale=iw/2:ih/2,paletteuse -y $FILE_PATH.gif
+FPS=20
+
+DIVIDE_SCALE=3
+
+SPEED_MAGNIFICATION=1.5
+
+
+ffmpeg -i $FILE_PATH.mov \
+  -vf fps=$FPS,scale=iw/$DIVIDE_SCALE:ih/$DIVIDE_SCALE,palettegen=stats_mode=diff \
+  -y $FILE_PATH.png \
+  && ffmpeg -i $FILE_PATH.mov \
+    -i $FILE_PATH.png \
+    -lavfi fps=$FPS,scale=iw/$DIVIDE_SCALE:ih/$DIVIDE_SCALE,paletteuse,setpts=PTS/$SPEED_MAGNIFICATION \
+    -y $FILE_PATH.gif
