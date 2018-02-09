6a321c5abbb2a5499ac080e52742f2e59b0cdc73
gates1de <g031i043@gmail.com>
2018-02-09 23:58:08 +0900

    Modify mysqldump_export.sh & Add get_aws_ip_ranges.sh

---
+++ aws/get_aws_ip_ranges.sh
@@ -0,0 +1,1 @@
+curl https://ip-ranges.amazonaws.com/ip-ranges.json > ip-ranges.json && cat ip-ranges.json
--- mysql/mysqldump_export.sh
+++ mysql/mysqldump_export.sh
@@ -1,5 +1,6 @@
 USER=$1
 DB=$2
 TABLE=$3
+TARGET_DIR=$4
 
-mysqldump -u $USER -p $DB $TABLE --tab=~/Downloads --fields-terminated-by=',' --fields-optionally-enclosed-by='"' -t --skip-triggers
+mysqldump -u $USER -p $DB $TABLE --tab=$TARGET_DIR --fields-terminated-by=',' --fields-optionally-enclosed-by='"' -t --skip-triggers
